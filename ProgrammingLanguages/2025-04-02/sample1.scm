137b87d8b73703e116ae8910acc679a560dbf88f
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2025-04-02 10:50:56 -0500

    Scripted auto-commit on change (2025-04-02 10:50:56) by gitwatch.sh

--- ProgrammingLanguages/2025-04-02/sample1.scm
+++ ProgrammingLanguages/2025-04-02/sample1.scm
@@ -7,3 +7,16 @@
 ;;; Dave knows best:
 (define (my-other-add x y)
   (+ x y))
+
+;;; Write some recursive code
+;;; Write recursive factorial
+;;; 5! = 5 * 4 * 3 * 2 * 1
+;;; two cases:
+;;; base case, easy simple no work one
+;;; recursive case, which refers backwards
+(define fact
+  (lambda (n)
+    (if (equal? n 1)
+	1
+	(* n (fact (- n 1))))))
+    
