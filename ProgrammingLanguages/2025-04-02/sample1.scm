5dc7fbe14d27f3ff0a19084c3cf5f47eabb32b97
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2025-04-02 10:41:20 -0500

    Scripted auto-commit on change (2025-04-02 10:41:20) by gitwatch.sh

--- ProgrammingLanguages/2025-04-02/sample1.scm
+++ ProgrammingLanguages/2025-04-02/sample1.scm
@@ -1,3 +1,9 @@
 (define my-add
   (lambda (x y)
     (+ x y)))
+
+;;; Shorthand that Dave believes interferes
+;;; with your learning, so avoid it, because
+;;; Dave knows best:
+(define my-other-add (x y)
+  (+ x y))
