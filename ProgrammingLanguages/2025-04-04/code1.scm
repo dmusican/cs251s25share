9d874427b2309a00c92fc152d175ea8563f8a277
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2025-04-04 09:53:29 -0500

    Scripted auto-commit on change (2025-04-04 09:53:29) by gitwatch.sh

--- ProgrammingLanguages/2025-04-04/code1.scm
+++ ProgrammingLanguages/2025-04-04/code1.scm
@@ -1,6 +1,12 @@
 ;;; select nth item in a list
+;; (define nth
+;;   (lambda (lst n)
+;;     (cond ( (null? lst)   #f  )
+;; 	  ( (equal? n 0)  (car lst)    )
+;; 	  ( else     (nth (cdr lst) (- n 1))))))
+
 (define nth
   (lambda (lst n)
-    (cond ( (null? lst)   #f  )
-	  ( (equal? n 0)  (car lst)    )
-	  ( else     (nth (cdr lst) (- n 1))))))
+    (cond ((null? lst) #f)
+	  ((equal? n 0) (car lst))
+	  (else (nth (cdr lst) (- n 1))))))
