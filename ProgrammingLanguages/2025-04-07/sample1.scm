71e7c07f51ebe80b714ecdb1f6c3dc9b58adc0ac
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2025-04-07 10:02:15 -0500

    Scripted auto-commit on change (2025-04-07 10:02:15) by gitwatch.sh

--- ProgrammingLanguages/2025-04-07/sample1.scm
+++ ProgrammingLanguages/2025-04-07/sample1.scm
@@ -1,4 +1,16 @@
 ;;; regular non-curried function
+;; (define mult
+;;   (lambda (a b)
+;;     (* a b)))
+
+;; redoit in a curried fashion
 (define mult
-  (lambda (a b)
-    (* a b)))
+  (lambda (a)
+
+    ;; return another function of one parameter
+    ;; that will multiply the two numbers
+    (lambda (b)
+      (* a b))
+
+   )
+)
