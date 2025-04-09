058a2456dfe82c2315a5dc6725b098453846bb51
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2025-04-09 10:53:50 -0500

    Scripted auto-commit on change (2025-04-09 10:53:50) by gitwatch.sh

--- ProgrammingLanguages/2025-04-09/example2.py
+++ ProgrammingLanguages/2025-04-09/example2.py
@@ -10,3 +10,21 @@ items = [1,2,3]
 new_items = remove_last(items)
 print(items)
 print(new_items)
+
+# Another version with lots of side effects
+# (when viewed globally)
+count = 0
+def remove_last_again(lst):
+    global count
+    count += 1    # side effect
+    lst.pop()    # side effect, changes the object
+    print("working")   # side effect
+    done = True
+    return None
+
+items = [5,6,7]
+print(count)
+print(items)
+remove_last_again(items)
+print(count)
+print(items)
