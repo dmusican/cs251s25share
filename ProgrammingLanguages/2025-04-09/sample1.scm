8b3ee88edb5008fa0ca812e8a0a23e2f41041cc2
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2025-04-09 10:02:08 -0500

    Scripted auto-commit on change (2025-04-09 10:02:08) by gitwatch.sh

--- ProgrammingLanguages/2025-04-09/sample1.scm
+++ ProgrammingLanguages/2025-04-09/sample1.scm
@@ -4,3 +4,18 @@
     (if (equal? start stop)
         (list start)
         (cons start (gen-list (+ start 1) stop)))))
+
+;;; lazy evaluation
+;;; instead of generating whole list we will instea
+;;; generate just a single Scheme pair (a cons cell)
+;;; where the car is the first number
+;;; and the cdr is a function (that we when we run it
+;;; we get the next cell in the lazy list)
+
+(define gen-lazy-list
+  (lambda (start stop)
+    (if (> start stop)
+        '()
+        (cons start
+              (lambda ()
+                (gen-lazy-list (+ start 1) stop))))))
