7cedd778fffa959a07dcc878180e2f615be4c26d
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2025-04-14 10:09:57 -0500

    Scripted auto-commit on change (2025-04-14 10:09:57) by gitwatch.sh

---
+++ ProgrammingLanguages/2025-04-14/prog1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int *x;
+    x = malloc(sizeof(int));
+    *x = 6;
+    printf("%i\n", *x);
+    int *y;
+    y = x;
+    printf("%i\n", *y);
+    *x = 12;
+    printf("%i\n", *x);
+    printf("%i\n", *y);
+    x = 19;
+    printf("%i\n", *x);
+}
