c701f62adc2554b97bc26909697fff271ef5a780
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2025-04-18 09:51:26 -0500

    Scripted auto-commit on change (2025-04-18 09:51:26) by gitwatch.sh

--- ProgrammingLanguages/2025-04-18/arrays.c
+++ ProgrammingLanguages/2025-04-18/arrays.c
@@ -16,4 +16,14 @@ int main() {
     int b[3]; // makes a whole new array
     // This is a failed attempt at a copy
     // b = a;
+    // To copy, could just loop and copy every item
+    // one by one, or can do...
+    // copy into b, from a, number of bytes
+    memcpy(b, a, sizeof(int)*3)
+
+
+    for (int i = 0; i < 3; i++) {
+        printf("b[%i] = %i\n", i, b[i]);
+    }
+
 }
