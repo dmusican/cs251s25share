cdcdba567640a2504fc579d5ed8e42e645a0aa22
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2025-04-18 10:12:30 -0500

    Scripted auto-commit on change (2025-04-18 10:12:30) by gitwatch.sh

---
+++ ProgrammingLanguages/2025-04-18/stringfun.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    // Slow way to make a string, but it works
+    char word[3];
+    word[0] = 'h';
+    word[1] = 'i';
+    word[2] = '\0';
+    printf("%s\n", word);
+}
