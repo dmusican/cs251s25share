723fa616846f0ab07df0c932e4770aa7f55f8028
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2025-04-18 09:45:39 -0500

    Scripted auto-commit on change (2025-04-18 09:45:39) by gitwatch.sh

--- ProgrammingLanguages/2025-04-18/arrays.c
+++ ProgrammingLanguages/2025-04-18/arrays.c
@@ -4,3 +4,10 @@
 int main() {
     // Step 1
     int a[3];
+    for (int i = 0; i < 3; i++) {
+        a[i] = i;
+    }
+
+    for (int i = 0; i < 3; i++) {
+        printf("a[%i] = %i\n", i, a[i]);
+    }
