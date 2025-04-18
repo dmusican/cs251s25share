a8a0f4dfec2acfdeb8e82eca43a32b9155156fc5
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2025-04-18 10:18:39 -0500

    Scripted auto-commit on change (2025-04-18 10:18:39) by gitwatch.sh

--- ProgrammingLanguages/2025-04-18/stringfun.c
+++ ProgrammingLanguages/2025-04-18/stringfun.c
@@ -3,11 +3,17 @@
 #include <string.h>
 
 int main() {
-    // Slow way to make a string, but it works
-    char word[2];
+    // BAD BAD BAD
+    // // Slow way to make a string, but it works
+    // char word[2];
+    // word[0] = 'h';
+    // word[1] = 'i';
+    // // word[2] = '\0';  B
+
+    // Better: correct, but long, and idiomatic
+    char word[3];
     word[0] = 'h';
     word[1] = 'i';
-    // word[2] = '\0';
-
+    word[2] = '\0';
     printf("%s\n", word);
 }
