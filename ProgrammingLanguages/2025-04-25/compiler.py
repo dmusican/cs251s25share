a65a6560f778cb3e90ae53ad62c0d0117757c523
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2025-04-25 09:51:15 -0500

    Scripted auto-commit on change (2025-04-25 09:51:15) by gitwatch.sh

--- ProgrammingLanguages/2025-04-25/compiler.py
+++ ProgrammingLanguages/2025-04-25/compiler.py
@@ -3,3 +3,9 @@ outfile = open('myprog.bash', 'w')
 
 for line in inpfile:
     (command, number) = line.split()
+    outfile.write('for i in {' + number + '}\n')
+    outfile.write('do\n')
+    outfile.write('    echo "' + command + '"\n')
+    outfile.write('done\n')
+inpfile.close()
+outfile.close()
