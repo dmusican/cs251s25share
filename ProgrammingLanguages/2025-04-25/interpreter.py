d5619fbab47a718a952ddcf7e7a67c29f9f4a65e
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2025-04-25 09:58:18 -0500

    Scripted auto-commit on change (2025-04-25 09:58:18) by gitwatch.sh

--- ProgrammingLanguages/2025-04-25/interpreter.py
+++ ProgrammingLanguages/2025-04-25/interpreter.py
@@ -2,7 +2,7 @@ inpfile = open('myprog.greet', 'r')
 
 for line in inpfile:
     (command, number) = line.split()
-    for i in range(number):
+    for i in range(int(number)):
         print(command)
 
 inpfile.close()
