6babefe6be127d863afbf704ac1ef8caa7d03b7c
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2025-05-14 10:58:42 -0500

    Scripted auto-commit on change (2025-05-14 10:58:42) by gitwatch.sh

--- ProgrammingLanguages/2025-05-14/SortFun.java
+++ ProgrammingLanguages/2025-05-14/SortFun.java
@@ -4,7 +4,7 @@ public class SortFun {
     public static void main(String[] args) {
         String[] words = {"a", "horse", "is", "happiest", "now"};
         System.out.println(Arrays.toString(words));
-        Arrays.sort(words);
+        Arrays.sort(words, (a,b) -> a.length() - b.length());
         System.out.println(Arrays.toString(words));
     }
 }
