e9d1656aaa7870e0a5f38c609838d13dc8690866
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2025-05-14 10:53:16 -0500

    Scripted auto-commit on change (2025-05-14 10:53:16) by gitwatch.sh

--- ProgrammingLanguages/2025-05-14/lambdafun.py
+++ ProgrammingLanguages/2025-05-14/lambdafun.py
@@ -7,3 +7,13 @@ items = [('horse', 9), ('cow', 3),
 print(sorted(items))
 
 print(sorted(items, key=lambda animal: animal[1]))
+
+# Make a new list as a results of adding 1 to each
+# item in a given list
+
+# boring way
+items = [9, 2, 1, 3, 5]
+new_items = items.copy()
+for i in range(len(new_items)):
+    new_items[i] += 1
+print(new_items)
