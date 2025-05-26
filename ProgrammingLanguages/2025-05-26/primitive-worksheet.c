dd1d3bce93877ea72db260e2cdce3c4b10645348
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2025-05-26 10:27:55 -0500

    Scripted auto-commit on change (2025-05-26 10:27:55) by gitwatch.sh

---
+++ ProgrammingLanguages/2025-05-26/primitive-worksheet.c
@@ -0,0 +1,46 @@
+
+typedef struct SchemeVal {
+    objectType type;
+    union {
+        ...
+        struct SchemeVal *(*pf)(struct SchemeVal *);
+    };
+} SchemeVal;
+
+SchemeVal *primitiveExp(SchemeVal *args) {
+   // check that args has length one and car(args) is numerical
+   // assume that car(args) is of type double, should check that as well
+   SchemeVal *result = talloc(sizeof(__________________________));
+
+   result->type = _____________________________________;
+
+   // Uses built-in C function exp
+
+   result->d = exp(__________________________);
+   return result;
+}
+
+void bind(char *name, SchemeVal *(*function)(SchemeVal *), Frame *frame) {
+    // Add primitive functions to top-level bindings list
+    SchemeVal *value = talloc(sizeof(________________________));
+
+    value->type = _________________________________________;
+
+    value->pf = ______________________________________;
+
+    // Your code will differ from each other some on the following;
+    // it adds a binding to a frame; can leave that out here
+    frame->bindings = ....;
+}
+
+void interpret(SchemeVal *tree) {
+
+    // Make top-level bindings list
+    Frame *frame = talloc(sizeof(Frame));
+    frame->bindings = makeEmpty();
+    frame->parent = NULL;
+
+    bind("exp", _______________, __________________);
+
+    // ...
+}
