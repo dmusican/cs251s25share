16cf3fe82d6627fa683a7f7f480f6f8c666bdf2b
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2025-05-28 10:07:03 -0500

    Scripted auto-commit on change (2025-05-28 10:07:03) by gitwatch.sh

--- ProgrammingLanguages/2025-05-28/somescheme.scm
+++ ProgrammingLanguages/2025-05-28/somescheme.scm
@@ -16,3 +16,13 @@
 ;; let* means do the above nested lets
 (let* ((a 3) (b (+ a 1)))
   (+ a b))
+
+;; Nested functions
+;; A common idiom in Scheme is to use let
+;; to make a "private" function, like a
+;; function you never intend to use anywhere
+;; else
+
+(let ((add-one (lambda (x)
+                 (+ x 1))))
+  (add-one 5)
