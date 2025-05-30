741b7b21424b75a35f2d8bde257e860e740b358f
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2025-05-30 09:52:57 -0500

    Scripted auto-commit on change (2025-05-30 09:52:57) by gitwatch.sh

---
+++ ProgrammingLanguages/2025-05-30/Types.java
@@ -0,0 +1,9 @@
+class Types {
+    public static void main(String[] args) {
+        int x = 3;
+        System.out.println("hi");
+        if (true == true) {
+            System.out.println(x.length());
+        }
+    }
+}
